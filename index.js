254072b477c4b2459d766ee021e87bcdc1fe3d6f
Denise Pendleton <dpendl1@hotmail.com>
2018-05-27 22:23:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Code your solution in this file.
 const drivers = ["Ray", "Ana", "Dan"];
+const oDrivers = ["Ray Charles", "Dan Man", "Ana Banana"]
 
 function lowerCaseDrivers(drivers){
 let  newDriver = drivers.map(function(driver){
     return driver.toLowerCase()
   })
   return newDriver
+}
+
+function nameToAttributes(oDrivers){
+  let newDriver = 
 }
